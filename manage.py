892222f1981f27b5f3b562a341caa455fb82e00d
edgaras <navedgaras@gmail.com>
2020-05-28 22:33:07 +0300

    5th try

--- manage.py
+++ manage.py
@@ -3,10 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    if os.environ.get('DJANGO_ENV') == 'production':
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azuresite.production')
-    else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azuresite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_management_system.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
